6ca6a7ad87e510ba11a9004859e14211db589f5f
Megan Brown <uxtechnologist@gmail.com>
2023-03-27 19:29:23 -0700

    Add dice properties and roll method

--- dice.py
+++ dice.py
@@ -3,10 +3,39 @@
     CS 5001 - Final Project
     Dice class
 """
+import random
 
 
 class Dice(object):
     """
-    Attributes: sides
+    Attributes: name, sides
     Methods: roll
     """
+    def __init__(self, name: str, sides: int = 6):
+        self.__name = name
+        self.__sides = sides
+
+
+    @property
+    def name(self) -> str:
+        # gets name of dice
+        return self.__name
+
+
+    @property
+    def sides(self) -> int:
+        # gets number of sides
+        return self.__sides
+
+
+    def __str__(self) -> str:
+        # prints info about dice object
+        info = "Dice name: " + self.__name + "\n" \
+                "Number of sides: " + str(self.__sides)
+        return info
+
+
+    def roll(self) -> int:
+        # rolls the dice
+        outcome = random.randint(1, self.__sides)
+        return outcome
